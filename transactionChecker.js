e441707779734ae7ef81c44274ab61b84bb7b1b2
xovest <79008917+xovest@users.noreply.github.com>
2022-01-07 15:51:01 +0200

    trans check

---
+++ transactionChecker.js
@@ -0,0 +1,42 @@
+const Web3 = require('web3');
+
+class TransactionChecker {
+    web3;
+    web3ws;
+    account;
+    subscription;
+
+    constructor(projectId, account) {
+        this.web3ws = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/' + projectId));
+        this.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + projectId));
+        this.account = account.toLowerCase();
+    }
+
+    subscribe(topic) {
+        this.subscription = this.web3ws.eth.subscribe(topic, (err, res) => {
+            if (err) console.error(err);
+        });
+    }
+
+    watchTransactions() {
+        console.log('Watching all pending transactions...');
+        this.subscription.on('data', (txHash) => {
+            setTimeout(async () => {
+                try {
+                    let tx = await this.web3.eth.getTransaction(txHash);
+                    if (tx != null) {
+                        if (this.account == tx.to.toLowerCase()) {
+                            console.log({address: tx.from, value: this.web3.utils.fromWei(tx.value, 'ether'), timestamp: new Date()});
+                        }
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            }, 60000)
+        });
+    }
+}
+
+let txChecker = new TransactionChecker(process.env.INFURA_ID, '0xe1Dd30fecAb8a63105F2C035B084BfC6Ca5B1493');
+txChecker.subscribe('pendingTransactions');
+txChecker.watchTransactions();
